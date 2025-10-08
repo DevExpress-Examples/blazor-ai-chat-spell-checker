c598658d593134d440d66428351cb5fd138c26a3
Schulllz <anton.shapovalov@devexpress.com>
2025-10-08 12:48:25 +0400

    added Blazor project with the implementation

---
+++ CS/Program.cs
@@ -0,0 +1,44 @@
+﻿using AiGrammarChecker.Components;
+using Microsoft.Extensions.AI;
+using OpenAI;
+
+string OpenAIKey = Environment.GetEnvironmentVariable("OPENAI_KEY");
+string ModelId = "gpt-4o-mini";
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDevExpressBlazor(options =>
+{
+    options.SizeMode = DevExpress.Blazor.SizeMode.Medium;
+});
+builder.Services.AddMvc();
+
+var chatClient = new OpenAIClient(OpenAIKey)
+    .GetChatClient(ModelId)
+    .AsIChatClient();
+
+builder.Services.AddScoped<IChatClient>((provider) => chatClient);
+builder.Services.AddDevExpressAI();
+
+var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AllowAnonymous();
+
+app.Run();
